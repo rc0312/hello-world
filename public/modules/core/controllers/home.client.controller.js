6c6ff30b2cca7302f65fb428f4d9ae39f07282b7
hoanghphan <hoangphan@Hoangs-MacBook-Air.local>
2015-06-27 14:54:17 -0700

    hpupdate

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -3,7 +3,37 @@
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
+		// Scope
+		$scope.events = [
+			{name: "Event 1", owner: "Dave"},
+			{name: "Event 2", owner: "Joe"},
+			{name: "Event 3", owner: "Tom"},
+		];
+
+		$scope.addEvent = function () {
+			$scope.events.push({name: $scope.newEvent, owner: $scope.newOwner});
+			$scope.newEvent = "";
+			$scope.newOwner = "";
+			$scope.eventDisplay.creatingEvent = false;
+		};
+
+		$scope.eventDisplay = {
+			name: null,
+			owner: null,
+			creatingEvent: false,
+		};
+
+		$scope.createEvent = function (event) {
+			$scope.eventDisplay.creatingEvent = true;
+		};
+
+		$scope.updateEvent = function (event) {
+			$scope.eventDisplay.name = event.name;
+			$scope.eventDisplay.owner = event.owner;
+		};
+
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
 	}
 ]);
